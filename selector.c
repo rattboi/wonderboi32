bff6a3824bdf330e94aabaa5ffbb5798cf91f3b8
Bradon Kanyid <bradon@kanyid.org>
2004-10-15 17:24:17 +0000

    Added the nice pretty windows code to the file selector, also moved things just a bit

--- selector.c
+++ selector.c
@@ -7,7 +7,14 @@
 #include "OKF/graphics16.h"
 #include "OKF/fileSelector.h"
 
-extern unsigned short WBTitle[];
+#include "file_dialog.h"
+
+#include "types.h"
+
+extern GPDRAWSURFACE gtSurface[2];
+extern int giSurface;
+
+uint16 FileBG[320*240];						// holds the file manager background;
 
 int DoFileSelector(char *filename)
 {
@@ -16,22 +23,30 @@ int DoFileSelector(char *filename)
 	
 	FileSelectorInit(&fileSelector);
 
+	Cls(giSurface);
+
+	DrawWindow(5, 9,  305, 15,  giSurface, COLOR_RED, COLOR_BLUE); // path window
+	DrawWindow(5, 30, 305, 160, giSurface, COLOR_RED, COLOR_BLUE); // file selector window
+	DrawWindow(5, 219,  305, 15,  giSurface, COLOR_RED, COLOR_BLUE); // totals window
+
+	SaveBackground(giSurface, &FileBG);
+
 	gm_strcpy(fileSelector.currentPath, "gp:\\gpmm\\wb32\\rom\\");
 
 	fileSelector.pTotalsFormat = "%s bytes, %s files";
 	fileSelector.pFileSizeFormat = " %s";
 
 	fileSelector.pathLeft = 12;
-	fileSelector.pathTop  = 6;
-	fileSelector.listTop  = 40;
+	fileSelector.pathTop  = 10;
+	fileSelector.listTop  = 37;
 	fileSelector.listSize = 10;
-	fileSelector.fileLeft = 20;
+	fileSelector.fileLeft = 25;
 	fileSelector.fileYDisplacement = 0;
 	fileSelector.sizeRight = 290;
 	fileSelector.sizeYDisplacement = 0;
 	fileSelector.totalsLeft = 12;
 	fileSelector.totalsTop = 220;
-	fileSelector.scrollBarLeft = 5;
+	fileSelector.scrollBarLeft = 13;
 	fileSelector.selectionHeight = 15;
 	fileSelector.selectionMargin = 3;
 	
@@ -44,7 +59,7 @@ int DoFileSelector(char *filename)
 //	fileSelector.sizesFont = 6;
 //	fileSelector.totalsFont = 5;
 	
-	fileSelector.pBackground = (unsigned short *) WBTitle;
+	fileSelector.pBackground = (unsigned short *) FileBG;
 //	fileSelector.backColor = RGB(0, 0, 0);
 	fileSelector.selectionColor = RGB(00, 31, 00);
 //	fileSelector.selectionIntensity = 16;
